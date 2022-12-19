b530c96d9834c4ac86661e8042160a9ecc0350ab
mosafari <m.safari2680@gmail.com>
2022-12-19 11:32:46 +0330

    Database ORMs

--- JWT/app.py
+++ JWT/app.py
@@ -12,3 +12,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # creates SQLALCHEMY object
 db = SQLAlchemy(app)
+
+# Database ORMs
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(70), unique = True)
+    password = db.Column(db.String(80))
+    date = db.Column(db.TEXT, default='')
+    day = db.Column(db.String(20), default = '')
+    
