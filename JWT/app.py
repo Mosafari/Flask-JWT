9f4d91eaa0cbf671b12cce09c34cb4bcf9083275
mosafari <m.safari2680@gmail.com>
2022-12-19 11:36:55 +0330

    decorator for verifying the JWT

--- JWT/app.py
+++ JWT/app.py
@@ -1,7 +1,9 @@
 # import flask
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+# imports for PyJWT authentication
+import jwt
+from functools import wraps
 
 # creates Flask object
 app = Flask(__name__)
@@ -22,3 +24,30 @@ class User(db.Model):
     date = db.Column(db.TEXT, default='')
     day = db.Column(db.String(20), default = '')
     
+
+# decorator for verifying the JWT
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        # return 401 if token is not passed
+        if not token:
+            return jsonify({'message' : 'Token is missing !!'}), 401
+  
+        try:
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query\
+                .filter_by(email = data['email'])\
+                .first()
+        except:
+            return jsonify({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users contex to the routes
+        return  f(current_user, *args, **kwargs)
+  
+    return decorated
